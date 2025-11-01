c2a5ae90e4ec67065b23ec5d235c802ca3f77c5f
pmaruti289 <pmaruti289@gmail.com>
2025-11-01 19:40:45 +0530

    EP_03 Competed

--- App.js
+++ App.js
@@ -1,33 +1,41 @@
-const headingOne = document.createElement("h1");
-headingOne.innerHTML = "This is Heading Tag 1";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const headingTwo = document.createElement("h2");
-headingTwo.innerHTML = "This is Heading Tag 2";
-
-const headingThree = document.createElement("h3");
-headingThree.innerHTML = "This is Heading Tag 3";
-
-//Modern way:It takes "Node objects" divided by comma not "Strings"
-root.append(headingOne, headingTwo, headingThree);
-
-//Older way Only one function takes only one "Node Object" and not Strings
-root.appendChild(headingOne);
-root.appendChild(headingTwo);
-root.appendChild(headingThree);
-
+//React.createElement ==> ReactElement(Object) ==> HTML Element
 const p = React.createElement("div", { id: "01", className: "paraClass" }, [
   React.createElement(
     "p",
-    { id: "01", className: "paraClass" },
+    { id: "01", key: 1, className: "paraClass" },
     "This is the Paragraph tag created by React Element"
   ),
   React.createElement(
     "p",
-    { id: "02", className: "paraClass" },
+    { id: "02", key: 2, className: "paraClass" },
     "This is the Paragraph tag created by React Element"
   ),
 ]);
-
 console.log(p);
+
+//JSX Element (Uses Bable Package to Transfile the JSX Code JSEngine understandable code)===> ReactElement ===> HTMLElement(Render)
+const jsxHeading = <h1>This is the Heading by using JSX🚀</h1>;
+console.log(jsxHeading);
+
+//Functional Component:Normal JavaScript funcion which return some piece of JSX
+const HeadingComponent = () => {
+  return (
+    <div>
+      <h1>Hello from Functional Component with(MultilLine)</h1>
+      <h2>Hello from Functional Component with(Multiline 2)</h2>
+    </div>
+  );
+};
+
+const HeadingComponentSingleLine = () => (
+  <>
+    <HeadingComponent />
+    <h1>Hello from Functional Component with(SingleLine)</h1>
+  </>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(p);
+root.render(<HeadingComponentSingleLine />);
